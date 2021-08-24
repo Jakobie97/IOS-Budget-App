9f99373453aa5672bf657262847aab61287ae603
Jacob Stockwell <JacobtStockwell@gmail.com>
2021-08-24 09:31:31 -0500

    initial commit

--- My First App/ViewController.swift
+++ My First App/ViewController.swift
@@ -8,12 +8,43 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+   
+    @IBOutlet weak var budgetTxt: UITextField!
+    
+    @IBOutlet weak var rentTxt: UITextField!
+    @IBOutlet weak var UtliTxt: UITextField!
+    @IBOutlet weak var FoodTxt: UITextField!
+    @IBOutlet weak var SubTxt: UITextField!
+    @IBOutlet weak var luxTxt: UITextField!
+    
+    
+    @IBOutlet weak var totalLabel: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        
+        
+       
     }
-
-
+   
+    @IBAction func CalculateBtn(_ sender: Any) {
+        
+        let budget = Double(budgetTxt.text!)!
+        let rent = Double(rentTxt.text!)!
+        let utility = Double(UtliTxt.text!)!
+        let food = Double(FoodTxt.text!)!
+        let sub = Double(SubTxt.text!)!
+        let lux = Double(luxTxt.text!)!
+        
+        let totalExpense = rent + utility + food + sub + lux
+        let totalResult = budget - totalExpense
+        
+        totalLabel.text = "My Spendable Balance: $" + "\(totalResult)"
+        
+        
+    }
+    
 }
-
